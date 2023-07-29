a85ccaade450fd1f1fa1d9749549c53fe9f9f6df
Erik Hübl <this.crimson@gmail.com>
2023-07-29 18:51:52 +0200

    make the height difference work

--- Assets/Scripts/Tube.cs
+++ Assets/Scripts/Tube.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = System.Random;
 
 public class Tube : MonoBehaviour {
     [SerializeField] private float movementSpeed;
@@ -6,9 +8,25 @@ public class Tube : MonoBehaviour {
 
     private float _deleteTimer;
 
+    private void Start() {
+        HightDifference();
+    }
+
     private void Update() {
         transform.position += Vector3.left * (movementSpeed * Time.deltaTime);
 
+        WaitForDeletion();
+    }
+
+    private void HightDifference() {
+        Random random = new Random();
+        var hightDifference = random.Next(-2, 3);
+
+        transform.position =
+            new Vector3(transform.position.x, transform.position.y + hightDifference, transform.position.z);
+    }
+
+    private void WaitForDeletion() {
         _deleteTimer += Time.deltaTime;
 
         if (_deleteTimer > waitForDelete) {
